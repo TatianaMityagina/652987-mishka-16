f60ab7fc425296f3e2b59e799acfd361d4ce53a0
TatianaMityagina <394999325@mail.ru>
2019-05-13 18:27:30 +0300

    JS меню

--- source/js/index.js
+++ source/js/index.js
@@ -1,19 +1,19 @@
   // Меню
 
-var navMain = document.querySelector('.main-nav__list-wrapper');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav__list-wrapper");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav__list-wrapper--nojs');
+navMain.classList.remove("main-nav__list-wrapper--nojs");
 navToggle.classList.remove("main-nav__toggle--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav__toggle--open')) {
-    navMain.classList.remove('main-nav__toggle--open');
-    navMain.classList.add('main-nav__toggle--closed');
+navToggle.addEventListener("click", function() {
+  if (navToggle.classList.contains("main-nav__toggle--open")) {
+    navToggle.classList.remove("main-nav__toggle--open");
+    navToggle.classList.add("main-nav__toggle--closed");
     navMain.style.display = "block";
   } else {
-    navMain.classList.add('main-nav__toggle--open');
-    navMain.classList.remove('main-nav__toggle--closed');
+    navToggle.classList.add("main-nav__toggle--open");
+    navToggle.classList.remove("main-nav__toggle--closed");
     navMain.style.display = "none";
   }
 });

22b2b812d5dde14d4a22cff016495ec995b2b721
TatianaMityagina <394999325@mail.ru>
2019-05-13 14:39:31 +0300

    Готовое меню + JS

--- source/js/index.js
+++ source/js/index.js
@@ -1,16 +1,19 @@
   // Меню
 
-var navMain = document.querySelector('.main-nav');
+var navMain = document.querySelector('.main-nav__list-wrapper');
 var navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav__list-wrapper--nojs');
+navToggle.classList.remove("main-nav__toggle--nojs");
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__toggle--open')) {
+    navMain.classList.remove('main-nav__toggle--open');
+    navMain.classList.add('main-nav__toggle--closed');
+    navMain.style.display = "block";
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav__toggle--open');
+    navMain.classList.remove('main-nav__toggle--closed');
+    navMain.style.display = "none";
   }
 });
